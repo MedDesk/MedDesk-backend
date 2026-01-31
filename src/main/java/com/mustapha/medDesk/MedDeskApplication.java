ad803d5dbd88a0547eabb74cd3ec8a9692727d83
mustapha <mustaphaamoutaki@gmail.com>
2026-01-31 15:07:10 +0100

    feat: create the structure of the project

---
+++ src/main/java/com/mustapha/medDesk/MedDeskApplication.java
@@ -0,0 +1,13 @@
+package com.mustapha.medDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustapha/medDesk/MedDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustapha.medDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
