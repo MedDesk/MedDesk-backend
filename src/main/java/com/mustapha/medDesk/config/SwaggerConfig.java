56dc9dbe77adc254ff604bb2016cc82462e8bfdb
mustapha <mustaphaamoutaki@gmail.com>
2026-01-31 16:06:04 +0100

    feat: add Swagger configuration

---
+++ src/main/java/com/mustapha/medDesk/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.mustapha.medDesk.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+
+
+}
