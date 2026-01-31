fcf06456779378abcd3452250cb938a1578323d7
mustapha <mustaphaamoutaki@gmail.com>
2026-01-31 16:12:54 +0100

    cnfg: simplify hashing and verifing passwords

--- src/main/java/com/mustapha/medDesk/util/PasswordUtil.java
+++ src/main/java/com/mustapha/medDesk/util/PasswordUtil.java
@@ -8,7 +8,7 @@ public class PasswordUtil {
 //    public static String hash(String password) {
 //        return BCrypt.hashpw(password, BCrypt.gensalt());
 //    }
-//
+
 //    public static boolean verify(String password, String hash) {
 //        return BCrypt.checkpw(password, hash);
 //    }
