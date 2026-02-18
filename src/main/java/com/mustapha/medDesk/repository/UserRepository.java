d5e95112b06ba4b3bd4fa3a1d9e8f65a03730c99
mustapha <mustaphaamoutaki@gmail.com>
2026-02-18 20:21:59 +0000

    feat: add user repsosiotry

---
+++ src/main/java/com/mustapha/medDesk/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.mustapha.medDesk.repository;
+
+import com.mustapha.medDesk.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+    User findByUsername(String username);
+}
