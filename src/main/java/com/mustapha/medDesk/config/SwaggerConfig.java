f11523c92543b514fdb25bd9a06b26f8e7db3a76
mustapha <mustaphaamoutaki@gmail.com>
2026-02-24 02:06:51 +0000

    fix: swagger documentation

--- src/main/java/com/mustapha/medDesk/config/SwaggerConfig.java
+++ src/main/java/com/mustapha/medDesk/config/SwaggerConfig.java
@@ -1,10 +1,20 @@
 package com.mustapha.medDesk.config;
 
-
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-// documenation swagger
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("medDesk API")
+                        .version("1.0")
+                        .description("Documentation API pour la gestion des clients"));
+    }
 }
