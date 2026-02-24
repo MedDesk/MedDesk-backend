e9ad0392266dfd137661629e02b20e5bdc3479a6
mustapha <mustaphaamoutaki@gmail.com>
2026-02-24 21:49:02 +0000

    test: test-al-enpoint-done-success

--- src/main/java/com/mustapha/medDesk/enums/StaffSpecialist.java
+++ src/main/java/com/mustapha/medDesk/enums/StaffSpecialist.java
@@ -1,8 +1,12 @@
 package com.mustapha.medDesk.enums;
 
 public enum StaffSpecialist {
-    NURSE,
-    TECH,
-    PHARMACIST,
-    LAB_TECH
+    GENERAL_PRACTITIONER,   // tabib 3am
+    CARDIOLOGIST,           // heart doc
+    DERMATOLOGIST,          // skin doc
+    PEDIATRICIAN,           // kids doc
+    ORTHOPEDIC_SURGEON,     // bons doc
+    RADIOLOGIST,            // achi3a
+    ANESTHESIOLOGIST,       // tkhdir
+    OTHER                   // default
 }
