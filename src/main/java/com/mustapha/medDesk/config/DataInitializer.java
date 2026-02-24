e32a24c21f928d240b9d8f1fad8b862cd7d177c7
mustapha <mustaphaamoutaki@gmail.com>
2026-02-24 01:43:08 +0000

    feat: create super admin as default account

--- src/main/java/com/mustapha/medDesk/config/DataInitializer.java
+++ src/main/java/com/mustapha/medDesk/config/DataInitializer.java
@@ -1,29 +1,38 @@
-//package com.mustapha.medDesk.config;
-//
-//import jakarta.annotation.PostConstruct;
-//import lombok.RequiredArgsConstructor;
-//
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class DataInitializer {
-//
-////    private final UserRepository userRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    @PostConstruct
-//    public void init() {
-////        if (userRepository.count() == 0) {   // only if database is empty
-////            User admin = new User();
-////            admin.setUsername("admin");
-////            admin.setPassword(passwordEncoder.encode("admin123"));
-////            admin.setRole(UserRole.ADMIN);
-////
-////            userRepository.save(admin);
-////            System.out.println(" Default ADMIN created: admin / admin123");
-////        }
-//    }
-//
-//}
+package com.mustapha.medDesk.config;
+
+import com.mustapha.medDesk.enums.Gender;
+import com.mustapha.medDesk.enums.UserRole;
+import com.mustapha.medDesk.model.User;
+import com.mustapha.medDesk.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void init(){
+        boolean isSuperAdminExists = userRepository.existsByRole(UserRole.SUPER_ADMIN);
+
+        if(!isSuperAdminExists){
+            User superAdmin = new User();
+            superAdmin.setFirstName("admin");
+            superAdmin.setLastName("admin");
+            superAdmin.setUsername("superadmin");
+            superAdmin.setPassword(passwordEncoder.encode("superadminpassword"));
+            superAdmin.setEmail("superadmin@gmail.com");
+            superAdmin.setGender(Gender.MALE);
+            superAdmin.setRole(UserRole.SUPER_ADMIN);
+
+            userRepository.save(superAdmin);
+            System.out.println("super admin account created successfully");
+        }
+    }
+
+}
