9033603db9470dfb89e7ebd8756ca17d425ff802
mustapha <mustaphaamoutaki@gmail.com>
2026-02-24 21:05:33 +0000

    feat: create staff mappe

--- src/main/java/com/mustapha/medDesk/mapper/StaffMapper.java
+++ src/main/java/com/mustapha/medDesk/mapper/StaffMapper.java
@@ -3,15 +3,27 @@ package com.mustapha.medDesk.mapper;
 import com.mustapha.medDesk.dto.request.staff.StaffDtoRequest;
 import com.mustapha.medDesk.dto.response.Staff.StaffDtoResponse;
 import com.mustapha.medDesk.model.Staff;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface StaffMapper {
 
+    @Mappings({
+            @Mapping(source = "staffSpecialist", target = "specialist"),
+            @Mapping(source = "CIN", target = "cin"),
+            @Mapping(source = "id", target = "id") ,// This works because Entity HAS an id
+            @Mapping(source = "role", target = "role")
+    })
     StaffDtoResponse toDto(Staff dto);
+
+
+    @Mappings({
+            @Mapping(source = "specialist", target = "staffSpecialist"),
+            @Mapping(source = "cin", target = "CIN"),
+            @Mapping(target = "id", ignore = true), // Explicitly ignore ID because we don't have id in our entity because we extend it form user
+            @Mapping(target = "cabinetId", ignore = true),
+            @Mapping(source = "role", target = "role"),
+    })
     Staff toEntity(StaffDtoRequest request);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
