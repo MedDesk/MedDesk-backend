72e5a0b52da2dabf4b626df730e430cbfa2dd332
mustapha <mustaphaamoutaki@gmail.com>
2026-02-26 14:29:07 +0000

    feat: create appointment repository

---
+++ src/main/java/com/mustapha/medDesk/repository/AppointmentReposiotry.java
@@ -0,0 +1,13 @@
+package com.mustapha.medDesk.repository;
+
+import com.mustapha.medDesk.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AppointmentReposiotry extends JpaRepository<Appointment, Long> {
+    Optional<Appointment> findById(Long id);
+    Optional<Appointment> findByEmail(String email);
+}
