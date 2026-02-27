2a1e180e1a5325ea47888f5bd027c6d7383b8b46
mustapha <mustaphaamoutaki@gmail.com>
2026-02-27 14:15:57 +0000

    fix: appointmentDtoRequest

--- src/main/java/com/mustapha/medDesk/dto/request/appointment/AppointmentDtoRequest.java
+++ src/main/java/com/mustapha/medDesk/dto/request/appointment/AppointmentDtoRequest.java
@@ -19,11 +19,6 @@ public class AppointmentDtoRequest {
     @NotNull(message = "Start time is required")
     private LocalTime scheduleTimeStart;
 
-    /*
-    REMOVED NOT NULL CONSTRAINT
-    We removed the validation here because this field is
-    calculated inside the setter based on the start time.
-    */
     private LocalTime scheduleTimeEnd;
 
     @NotNull(message = "Status is required")
@@ -36,12 +31,7 @@ public class AppointmentDtoRequest {
     private LocalDate appointmentDate;
 
     public void setScheduleTimeStart(LocalTime scheduleTimeStart) {
-        /*
-        AUTOMATIC CALCULATION
-        When the JSON is parsed and the start time is set
-        this method automatically adds thirty minutes to
-        define the duration of the appointment slot.
-        */
+
         this.scheduleTimeStart = scheduleTimeStart;
         if (scheduleTimeStart != null) {
             this.scheduleTimeEnd = scheduleTimeStart.plusMinutes(30);
