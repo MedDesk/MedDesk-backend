5721b3bbbbd0592d67df817fb1f798f788a67103
mustapha <mustaphaamoutaki@gmail.com>
2026-02-28 12:00:00 +0000

    fix errors

--- src/main/java/com/mustapha/medDesk/mapper/MedicalRecordMapper.java
+++ src/main/java/com/mustapha/medDesk/mapper/MedicalRecordMapper.java
@@ -32,4 +32,5 @@ public interface MedicalRecordMapper {
     @Mapping(target = "appointment", ignore = true)
     @Mapping(target = "vital", ignore = true)
     void updateMedicalRecordForm(MedicalRecordDtoRequest request, @MappingTarget MedicalRecord medicalRecord);
+
 }
