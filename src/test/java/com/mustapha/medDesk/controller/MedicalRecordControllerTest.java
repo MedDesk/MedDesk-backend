3a282a31bca5fbab17cdcd8dd33c484bbd64ed7a
mustapha <mustaphaamoutaki@gmail.com>
2026-03-15 20:48:04 +0000

    feat: create doctor It

---
+++ src/test/java/com/mustapha/medDesk/controller/MedicalRecordControllerTest.java
@@ -0,0 +1,32 @@
+package com.mustapha.medDesk.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedicalRecordControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAllByPatientId() {
+    }
+}
